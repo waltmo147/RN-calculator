76d65f1a95d8c04a864e0ea309148291263a1e8d
Walt Mo <apple@Walt-ModeiMac.local>
2018-12-05 23:48:38 -0500

    UI almost finished

--- App.js
+++ App.js
@@ -6,25 +6,70 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity
+} from "react-native";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+
+    // this.state
+  }
+
   render() {
+    let rows = [];
+    let nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [".", 0, "="]];
+
+    for (let i = 0; i < 4; i++) {
+      let row = [];
+      for (let j = 0; j < 3; j++) {
+        row.push(
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btntext}>{nums[i][j]}</Text>
+          </TouchableOpacity>
+        );
+      }
+      rows.push(<View style={styles.row}>{row}</View>);
+    }
+
+    let oper = ["+", "-", "*", "/"];
+    let ops = [];
+    for (let i = 0; i < 4; i++) {
+      ops.push(
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.white}>{oper[i]}</Text>
+        </TouchableOpacity>
+      );
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Hello world
-        </Text>
-
+        <View style={styles.result}>
+          <Text style={styles.resultText}>11 * 11</Text>
+        </View>
+        <View style={styles.calculation}>
+          <Text style={styles.calculationText}>11 * 11</Text>
+        </View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}>{rows}</View>
+          <View style={styles.operations}>{ops}</View>
+        </View>
       </View>
     );
   }
@@ -32,19 +77,60 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  btn: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "stretch"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  resultText: {
+    fontSize: 35,
+    color: "white"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  calculationText: {
+    fontSize: 27,
+    color: "white"
   },
+
+  btntext: {
+    fontSize: 30
+  },
+  white: {
+    color: "white",
+    fontSize: 30
+  },
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  result: {
+    flex: 2,
+    backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "flex-end"
+  },
+  calculation: {
+    flex: 1,
+    backgroundColor: "green",
+    justifyContent: "center",
+    alignItems: "flex-end"
+  },
+  buttons: {
+    flex: 7,
+    flexDirection: "row"
+  },
+  numbers: {
+    flex: 3,
+    backgroundColor: "yellow"
+  },
+
+  operations: {
+    flex: 1,
+    justifyContent: "space-around",
+    backgroundColor: "black"
+  }
 });
